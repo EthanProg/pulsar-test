fa5fe87698e49ef96f412705dbbfc0994bf75125
Ethan Li <programcccethan@gmail.com>
2023-11-20 16:43:12 +0800

    Initial commit

---
+++ src/test/java/com/test/pulsar/BasicTest.java
@@ -0,0 +1,91 @@
+package com.test.pulsar;
+
+import org.apache.pulsar.client.api.Consumer;
+import org.apache.pulsar.client.api.Message;
+import org.apache.pulsar.client.api.PulsarClientException;
+import org.apache.pulsar.client.api.Schema;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+/**
+ * Create the nodeport:
+ * <snippet>
+ * apiVersion: v1
+ * kind: Service
+ * metadata:
+ *   name: pulsar-broker-nodeport
+ *   namespace: lfp-pulsar
+ * spec:
+ *   ports:
+ *   - name: pulsar-nodeport
+ *     nodePort: 30363
+ *     port: 6651
+ *     protocol: TCP
+ *     targetPort: 6650
+ *   - name: http
+ *     nodePort: 30207
+ *     port: 8081
+ *     protocol: TCP
+ *     targetPort: 8080
+ *   selector:
+ *     lfp.app.name: pulsar-standalone
+ *   type: NodePort
+ * </snippet>
+ *
+ * Create a topic:
+ * <snippet>
+ *     curl -sL -X PUT "http://10.14.0.208:30207/admin/v2/tenants/test1" -H "Content-Type: application/json" --data-raw  "{\"allowedClusters\": [\"standalone\"]}"
+ * curl -sL -X PUT "http://10.14.0.208:30207/admin/v2/namespaces/test1/test-namespace" -H "Content-Type: application/json" --data-raw "{
+ *     \"retention_policies\": {\"retentionTimeInMinutes\": 1440, \"retentionSizeInMB\": 1024},
+ *     \"subscription_expiration_time_minutes\": 43200,
+ *     \"message_ttl_in_seconds\": 86400
+ *   }"
+ * </snippet>
+ *
+ */
+class BasicTest {
+    @Test
+    void test1() throws PulsarClientException {
+        var pulsarUrl = "pulsar://10.14.0.208:30363/";
+        var topic = "persistent://test1/test-namespace/topic1";
+
+        var pulsarClient = org.apache.pulsar.client.api.PulsarClient.builder().serviceUrl(pulsarUrl).build();
+        var producer = pulsarClient.newProducer(Schema.STRING).topic(topic).create();
+        var consumer = pulsarClient.newConsumer(Schema.STRING).topic(topic).subscriptionName("test-subscription").subscribe();
+
+        ackAllPreviousMesasges(consumer);
+
+        var id = String.valueOf(UUID.randomUUID());
+
+        producer.newMessage()
+                .key("document-compress-async")
+                .property("compressAsyncTraceId", id)
+                .value(id)
+                .send();
+
+        long start = System.currentTimeMillis();
+        var msg = consumer.receive();
+        String consumeId = msg.getValue();
+        long end = System.currentTimeMillis();
+        System.out.println("Time: " + (end - start));
+        consumer.acknowledgeCumulative(msg.getMessageId());
+
+        Assertions.assertEquals(id, consumeId);
+    }
+
+    void ackAllPreviousMesasges(Consumer<String> consumer) throws PulsarClientException {
+        while (true) {
+            Message<String> msg = consumer.receive();
+            try {
+                // Process the message
+                System.out.printf("Message received: %s", msg.getValue());
+                // Acknowledge the message cumulatively
+                consumer.acknowledgeCumulative(msg.getMessageId());
+            } catch (Exception e) {
+                // Handle the exception
+            }
+        }
+    }
+}
---
+++ src/test/java/com/test/pulsar/PulsarTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.test.pulsar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsarTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello World!");
+    }
+
+}
